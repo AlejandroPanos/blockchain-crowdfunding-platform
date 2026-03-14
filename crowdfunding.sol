324bf1ff028eb2305951a054e38edc760f2170c2
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-03-14 17:50:58 +0100

    update/init-declarations

--- crowdfunding.sol
+++ crowdfunding.sol
@@ -9,4 +9,43 @@ contract Crowdfunding {
     constructor() {
         owner = msg.sender;
     }
+
+    // Initial declarations
+    uint256 campaignIdCounter = 0;
+    struct Campaign {
+        uint256 id;
+        string title;
+        string description;
+        uint256 goalAmount;
+        uint256 totalPledged;
+        uint256 deadline;
+        Status status;
+        address creator;
+        bool fundsWithdrawn;
+        bool isFinalised;
+    }
+
+    enum Status {
+        Active,
+        Successful,
+        Failed, 
+        Cancelled
+    }
+
+    mapping(uint256 => Campaign) public campaigns;
+    mapping(uint256 => mapping(address => uint256)) public pledges;
+
+    // Modifier
+    modifier OnlyCreator(uint256 _campaignId) {
+        require(msg.sender == campaigns[_campaignId].creator, "You are not the creator of the campaign.");
+        _;
+    }
+
+    // Events
+    event NewCampaign(uint256);
+    event NewPledge(uint256, uint256);
+    event CampaignFinalised(uint256);
+    event FundsDistributed(uint256);
+    event CancelledCampaign(uint256);
+    event RefundIssued(uint256, uint256);
 }
